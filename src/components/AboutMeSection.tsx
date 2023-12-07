4288f746bbc6b5fdb5997eebe50a17fa541d1ffe
FACUNDEZ <facu.mdq@live.com>
2023-12-07 15:17:14 -0300

    .

--- src/components/AboutMeSection.tsx
+++ src/components/AboutMeSection.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 
 function AboutMe() {
@@ -17,7 +18,7 @@ function AboutMe() {
                         I am currently open to new job opportunities that allow me to apply and expand my knowledge. In addition to my interest in software development, I have a strong desire to inspire and support others in their technological journey. If you have questions, collaboration ideas, or just want to chat about development, feel free to contact me! I am eager to connect and share experiences in this exciting world of technology. 💬🚀
                     </p>
 
-                    <button className="block mt-14 lg:mt-12 ml-4 lg:mb-5 shadow-md rounded bg-cyan-500 text-white text-xl font-bold px-4 py-3 hover:bg-cyan-600 hover:-translate-y-3 duration-500">Contact Me</button>
+                    <button onClick={() => (window.location.href = "#contact")} className="block mt-14 lg:mt-12 ml-4 lg:mb-5 shadow-md rounded bg-cyan-500 text-white text-xl font-bold px-4 py-3 hover:bg-cyan-600 hover:-translate-y-3 duration-500">Contact Me</button>
                 </div>
                 <div className='pb-16 mt-6 lg:w-6/12 lg:mt-14'>
                     <h2 className='font-bold text-2xl lg:text-3xl ml-4 mb-10'>My Skills</h2>
